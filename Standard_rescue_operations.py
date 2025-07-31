79c2fe08ce2cf5c4201e0440e761528cf2751f68
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-08-01 00:48:47 +0330

    fix func get_input

--- Standard_rescue_operations.py
+++ Standard_rescue_operations.py
@@ -3,10 +3,12 @@ def get_input():
     B = int(input('Number of Space Stations on Earth :'))
     C = int(input('Number of Residential Area on Mars :'))
     Passengers_Groups = list(map(int, input('Number of Passengers Groups :').split(' ')))
-    Capacity_Residential_Area = list(map(int, input('Number of Capacity Residential Area :')))
-    Time_Texas_Space_Stations = list(map(int, input('Number of Time Texas Space Stations :')))
+    Capacity_Residential_Area = list(map(int, input('Number of Capacity Residential Area :').split(' ')))
+    Time_Texas_Space_Stations = list(map(int, input('Number of Time Texas Space Stations :').split(' ')))
     Time_of_Traveling = list()
     for i in range(B):
         matrix_Time = list(map(int, input('Matrix Time :').split(' ')))
         Time_of_Traveling.append(matrix_Time)
     return ship, B, C, Passengers_Groups, Capacity_Residential_Area, Time_Texas_Space_Stations, Time_of_Traveling
+
+ship, B, C, Passengers_Groups, Capacity_Residential_Area, Time_Texas_Space_Stations, Time_of_Traveling = get_input()
