ee48883b110bcd0f2981bf05078edb4794bed098
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-08-01 16:18:28 +0330

    rename vars

--- Standard_rescue_operations.py
+++ Standard_rescue_operations.py
@@ -2,13 +2,14 @@ def get_input():
     ship = int(input('Number of accessible Starships :'))
     B = int(input('Number of Space Stations on Earth :'))
     C = int(input('Number of Residential Area on Mars :'))
-    Passengers_Groups = list(map(int, input('Number of Passengers Groups :').split(' ')))
-    Capacity_Residential_Area = list(map(int, input('Number of Capacity Residential Area :').split(' ')))
-    Time_Texas_Space_Stations = list(map(int, input('Number of Time Texas Space Stations :').split(' ')))
-    Time_of_Traveling = list()
+    source = list(map(int, input('Number of Passengers Groups :').split(' ')))
+    dest = list(map(int, input('Number of Capacity Residential Area :').split(' ')))
+    init_time = list(map(int, input('Number of Time Texas Space Stations :').split(' ')))
+    travel_time = list()
     for i in range(B):
         matrix_Time = list(map(int, input('Matrix Time :').split(' ')))
-        Time_of_Traveling.append(matrix_Time)
-    return ship, B, C, Passengers_Groups, Capacity_Residential_Area, Time_Texas_Space_Stations, Time_of_Traveling
+        travel_time.append(matrix_Time)
+    return ship, B, C, source, dest, init_time, travel_time
 
-ship, B, C, Passengers_Groups, Capacity_Residential_Area, Time_Texas_Space_Stations, Time_of_Traveling = get_input()
+
+ship, B, C, source, dest, init_time, travel_time = get_input()
