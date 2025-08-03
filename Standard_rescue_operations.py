8a24a7a3ab56ea62f0c5d8c3c285aaa534b40517
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-08-03 11:21:16 +0330

    get_input

--- Standard_rescue_operations.py
+++ Standard_rescue_operations.py
@@ -30,3 +30,18 @@ def get_input():
     to_base = list(map(int, input().split()))
     travel_matrix = [list(map(int, input().split())) for _ in range(num_bases)]
     return num_ships, num_bases, num_colons, groups, capacities, to_base, travel_matrix
+
+
+if __name__ == '__main__':
+    # num_ships, num_bases, num_colons, base, capacities, to_base, travel_matrix = get_input()
+    num_ships, num_bases, num_colons, base, capacities, to_base, travel_matrix = (3, 3, 3, [1, 3, 3], [4, 4, 1], [7, 4, 9], [[6, 7, 8], [10, 9, 2], [6, 3, 7]])
+    tasks: List[Tuple[int, int, int]] = []
+
+
+    for b in range(num_bases):
+        for _ in range(base[b]):
+            c_best = min(range(num_colons), key=lambda c: travel_matrix[b][c])
+            tasks.append((b, c_best, travel_matrix[b][c_best]))
+            capacities[c_best] -= 1
+    base_to_colon = tasks
+
